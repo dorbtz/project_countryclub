c3fde8825b421ec00eb20f9a6e8208352a8d346a
dorbtz <93556351+dorbtz@users.noreply.github.com>
2022-03-21 16:53:20 +0200

    Update settings.py

--- project_countryclub/project_countryclub/settings.py
+++ project_countryclub/project_countryclub/settings.py
@@ -19,8 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-u=ho$w$n_omts!w9%(-_(%&-6z4@@v)gzhcvx*7cit8y-e!1dk'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
